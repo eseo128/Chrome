3f30ff3abb4ae5754351230cf867f317fdb99b1d
eseo128 <34049770+eseo128@users.noreply.github.com>
2021-08-09 20:22:24 +0900

    JS : 스크롤 시 다운로드 버튼 보이기

--- main.js
+++ main.js
@@ -1,1 +1,14 @@
 'use strict';
+
+//브라우저는 $(window)
+//스크롤 시 헤더의 다운로드 버튼 보이기
+$(function () {
+    $(window).scroll(function () {
+        if ($(window).scrollTop() > 500) {
+            $('.download').addClass('active');
+        }
+        else {
+            $('.download').removeClass('active');
+       }
+   }) 
+});
